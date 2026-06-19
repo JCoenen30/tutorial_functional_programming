b9afe8b97810978878e676740cd9a51caa9e86f8
JCoenen30 <jason.j.coenen@gmail.com>
2026-06-19 08:37:46 -0500

    advanced functions finished

--- 01_tutorials/03_advanced_function.R
+++ 01_tutorials/03_advanced_function.R
@@ -12,7 +12,7 @@
 library(tidyverse)
 
 ## Functions
-source("...")
+source("R/utils.R")
 
 # 2. Load data ------------------------------------------------------------
 
@@ -21,17 +21,46 @@ iris_tbl <- as_tibble(iris)
 
 # 3. Analyze data ---------------------------------------------------------
 
-## Calculate mean of numeric variables in Iris dataset, and sort them
-
+## Calculate mean of numeric variables in Iris dataset, and sort them (First lines commented out are what we used to build function)
+# iris_tbl |>
+#     summarise(
+#         across(
+#             where(is.numeric), mean
+#         ),
+#         .by = Species
+#     ) |>
+#     pivot_longer(
+#         cols = where(is.numeric),
+#         names_to = "measure",
+#         values_to = "mean"
+#     ) |> 
+#     group_by(measure) |>
+#     arrange(
+#         desc(mean),
+#         .by_group = TRUE
+#     ) |>
+#     ungroup()
+iris_tbl |>
+    calc_iris_mean()
 
 
 # 4. General function -----------------------------------------------------
 
 ## https://ggplot2.tidyverse.org/reference/tidyeval.html
 
+## FUNCTION
+
 ## Apply to other datasets
+calc_numeric_mean(
+    data = airquality,
+    group = Month
+) |> 
+    print(n = 50)
 
-
+calc_numeric_mean(
+    data = iris_tbl,
+    group = Species
+)
 
 
 
